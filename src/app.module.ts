45b7b307081bf16655c835402c8a5faf77ef4a29
Dedwison <dedwison@gmail.com>
2025-04-14 16:11:00 -0400

    proteger schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { join } from 'path';
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -14,14 +17,43 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
-      playground: false,
-      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
-      plugins: [
-        ApolloServerPluginLandingPageLocalDefault()
+      imports: [
+        /*importar módulos*/ 
+        AuthModule
       ],
+      inject: [
+        /*inyectar servicios*/
+        JwtService
+      ],
+      useFactory: async(jwtService: JwtService) => ({
+        playground: false,
+        autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault()
+        ],
+        context({ req }) {
+          const token = req.headers.authorization?.replace('Bearer ', '');
+          if(!token) throw new Error('Token needed')
+
+          //! Comentar para renovar el token
+          const payload = jwtService.decode( token )
+          if(!payload) throw new Error('Token not valid')
+        }
+      })
     }),
+
+    //? Configuración básica
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   playground: false,
+    //   autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+    //   plugins: [
+    //     ApolloServerPluginLandingPageLocalDefault()
+    //   ],
+    // }),
 
     TypeOrmModule.forRoot({
       type: 'postgres',
