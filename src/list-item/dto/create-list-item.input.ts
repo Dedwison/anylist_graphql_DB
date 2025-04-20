c6fa0783aedda6c76c55798dbc61025601ae0b08
Dedwison <dedwison@gmail.com>
2025-04-20 13:25:39 -0400

    insert listItem

--- src/list-item/dto/create-list-item.input.ts
+++ src/list-item/dto/create-list-item.input.ts
@@ -1,7 +1,28 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Int, Field, ID } from '@nestjs/graphql';
+import { IsBoolean, IsNumber, IsOptional, IsUUID, Min } from 'class-validator';
 
 @InputType()
 export class CreateListItemInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @Field( () => Number, { nullable: true } )
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  quantity: number = 0;
+
+  @Field( () => Boolean, { nullable: true } )
+  @IsOptional()
+  @IsBoolean()
+  completed: boolean = false;
+
+  @Field( () => ID )
+  @IsUUID()
+  listId: string;
+
+  @Field( () => ID )
+  @IsUUID()
+  itemId: string;
+
+
+
 }
--- src/list-item/list-item.service.ts
+++ src/list-item/list-item.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { ListItem } from './entities/list-item.entity';
+
 import { CreateListItemInput } from './dto/create-list-item.input';
 import { UpdateListItemInput } from './dto/update-list-item.input';
 
 @Injectable()
 export class ListItemService {
-  create(createListItemInput: CreateListItemInput) {
-    return 'This action adds a new listItem';
+
+  constructor(
+    @InjectRepository( ListItem )
+    private readonly listItemsRepository: Repository<ListItem>
+  ){}
+
+  async create(createListItemInput: CreateListItemInput): Promise<ListItem> {
+    const { itemId, listId, ...rest } = createListItemInput;
+    
+    const newListItem = this.listItemsRepository.create({
+      ...rest,
+      item: { id: itemId },
+      list: { id: listId }
+    })
+
+    return this.listItemsRepository.save( newListItem )
   }
 
   findAll() {
     return `This action returns all listItem`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} listItem`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} listItem`;
+  // }
 
-  update(id: number, updateListItemInput: UpdateListItemInput) {
-    return `This action updates a #${id} listItem`;
-  }
+  // update(id: number, updateListItemInput: UpdateListItemInput) {
+  //   return `This action updates a #${id} listItem`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} listItem`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} listItem`;
+  // }
 }
